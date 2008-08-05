475e936944883b14345f03a4460e1230bf7d67f9
Austin Putman <austin@radicaldesigns.org>
2008-08-04 18:15:30 -0700

    a draft of a deploy file

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,15 +1,35 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :application, "stasi"
+set :repository,  "git@github.com:austinfromboston/stasi.git"
+
+set :user, "stasi"
+default_run_options[:pty] = true
 
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
 # via the :deploy_to variable:
-# set :deploy_to, "/var/www/#{application}"
+set :deploy_to, "/home/stasi/stasi"
 
 # If you aren't using Subversion to manage your source code, specify
 # your SCM below:
-# set :scm, :subversion
+set :scm, :git
+set :deploy_via, :remote_cache
 
-role :app, "your app-server here"
-role :web, "your web-server here"
-role :db,  "your db-server here", :primary => true
+role :app, "zheng.radicaldesigns.org"
+role :web, "zheng.radicaldesigns.org"
+role :db,  "zheng.radicaldesigns.org", :primary => true
+
+after "deploy:update_code", "deploy:symlink_shared"
+
+namespace :deploy do
+  task :symlink_shared, :roles => :app, :except => {:no_symlink => true} do
+    invoke_command "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+    invoke_command "ln -nfs #{shared_path}/config/mongrel_cluster.yml #{release_path}/config/mongrel_cluster.yml"
+    invoke_command "ln -nfs #{shared_path}/config/contact_grudges_development.yml #{release_path}/config/contact_grudges_development.yml"
+    invoke_command "ln -nfs #{shared_path}/config/imported_contacts_and_projects.yml #{release_path}/config/imported_contacts_and_projects.yml"
+
+    # clear stylesheets folder
+    #invoke_command "rm #{release_path}/public/stylesheets/*css"
+  end
+
+
+end
